401d7a0f212f4522e622c90211afbfdf1565cef3
racketeers3 <kiloskilo123@gmail.com>
2026-03-07 20:38:41 -0500

    Fix React crash in production when Auth0 variables are missing

--- CityLens/src/main.tsx
+++ CityLens/src/main.tsx
@@ -4,14 +4,21 @@ import './index.css'
 import App from './App.tsx'
 import { Auth0Provider } from '@auth0/auth0-react'
 
+const auth0Domain = import.meta.env.VITE_AUTH0_DOMAIN;
+const auth0ClientId = import.meta.env.VITE_AUTH0_CLIENT_ID;
+
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
-    <Auth0Provider
-      domain={import.meta.env.VITE_AUTH0_DOMAIN}
-      clientId={import.meta.env.VITE_AUTH0_CLIENT_ID}
-      authorizationParams={{ redirect_uri: window.location.origin }}
-    >
+    {auth0Domain && auth0ClientId ? (
+      <Auth0Provider
+        domain={auth0Domain}
+        clientId={auth0ClientId}
+        authorizationParams={{ redirect_uri: window.location.origin }}
+      >
+        <App />
+      </Auth0Provider>
+    ) : (
       <App />
-    </Auth0Provider>
+    )}
   </StrictMode>,
 )
